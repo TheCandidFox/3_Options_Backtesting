9741129a511bd784585389c85db20676b5f5c345
David Fox <david.fox.dynamics@gmail.com>
2025-07-06 11:14:13 -0500

    file structure going forward with polygon

--- 0_Development/0_Modules/1_BackTrader_Getting_Started/First_Program.py
+++
@@ -1,5 +0,0 @@
-import backtrader as bt
-import pandas as pd
-import matplotlib.pyplot as plt
-
-print("All libraries loaded successfully.")
---
+++ 0_Development/0_Schwab/0_Getting_Started_SCHWAB/Init_Schwab_API.py
@@ -0,0 +1,29 @@
+import requests
+import base64
+
+APP_KEY = "XAuRzDiRG9z1jYnz8gkwcKZa6k3HHAVv"
+APP_SECRRET = "pHToyhfrHvtwrHMA"
+
+
+auth_url = f'https://api.schwabapi.com/v1/oauth/authorize?client_id={APP_KEY}&redirect_uri=https://127.0.0.1'
+
+print(f"Click to authenticate: {auth_url}")
+
+returned_link = input("Parse the redirect URL here: ")
+
+code =  f"{returned_link[returned_link.index('code=')+5:returned_link.index('%40')]}@"
+
+headers = {'Authorization': f'Basic {base64.b64encode(bytes(f"{APP_KEY}:{APP_SECRRET}", "utf-8")).decode("utf-8")}', 'Content-Type': 'application/x-www-form-urlencoded'}
+
+data = {'grant_type': 'authorization_code', 'code': code, 'redirect_uri': 'https://127.0.0.1'}
+
+response = requests.post(url= 'https://api.schwabapi.com/v1/oauth/token', headers=headers, data=data)
+
+tD = response.json()
+access_token = tD['access_token']
+refresh_token = tD['refresh_token']
+
+base_url = "https://api.schwabapi.com/trader/v1"
+
+response =  requests.get(f'{base_url}/accounts/accountNumbers', headers={'Authorization': f'Bearer {access_token}'})
+print(response.json())
